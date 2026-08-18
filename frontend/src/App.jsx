e6df1b923f180e4dd22eed54f2ef0e8bc220b10c
EstakuyoExakt <jeustaquio@exakt.com.ph>
2026-08-18 15:57:46 +0800

    First Commit - Setup Project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <h1 className='text-blue-500 font-bold text-7xl'>Hello Exakt Inventory System</h1>
+    </div>
+  )
+}
+
+export default App
